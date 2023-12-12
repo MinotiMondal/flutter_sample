d9a807c536f56a3af9a4538318cb8ad45e1c5aa9
satish <satish.kumar@jungleegames.com>
2023-12-12 09:49:50 +0530

    refactored

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ import 'package:sample/repos/repositories.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  RepositoryProvider.value(value: UserRepository());
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //This widget is the root of your application
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
