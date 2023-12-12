67f44a72f6553db0ef62ea0c5477f3716e30f2a8
satish <satish.kumar@jungleegames.com>
2023-12-12 10:22:40 +0530

    minor fixed

--- lib/repositories/base_http.dart
+++ lib/repositories/base_http.dart
@@ -2,12 +2,12 @@ import 'package:http/http.dart';
 import 'package:sample/api/api.dart';
 
 abstract class BaseHTTP {
-  Future<dynamic> sendRequest(HttpType httpType, String endpotin,
+  Future<dynamic> sendRequest(HttpType httpType, String endpoint,
       {Map<String, dynamic>? body}) async {
     //do similarly for other methods
     switch (httpType) {
       case HttpType.get:
-        return await get(Uri.parse(Api.api + endpotin));
+        return await get(Uri.parse(Api.api + endpoint));
       case HttpType.delete:
       case HttpType.post:
     }
